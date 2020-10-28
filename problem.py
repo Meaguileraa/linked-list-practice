a4990e22b75786835e7174039d9c87802de167f9
Marissa Aguilera <marissaguilera@yahoo.com>
2020-10-27 18:52:47 -0700

    File and instructions

---
+++ problem.py
@@ -0,0 +1,11 @@
+
+
+#Create a trick or treat basket using object oriented programming.
+# Create a trick or treat bag
+# Add candy to the bag 
+
+
+#Notes: 
+#Data Tables: Bag(bag_id, user_id), 
+# BagCandy(bag_candy__id, bag_id, candy_id), 
+# Candy(candy_id, amount)
